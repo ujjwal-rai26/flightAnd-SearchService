36bda9766f2ca7bf472af3250d53d6f19e627bfb
ujjwal rai <ujjwalrai26092001@gmail.com>
2023-04-27 18:20:55 +0530

    added flight create middlewares

---
+++ src/middlewares/flight-middlewares.js
@@ -0,0 +1,26 @@
+const validateCreateFlight=(req,res,next)=>{
+if(  
+   !req.body.flightNumber ||
+   !req.body.airplaneId ||
+   !req.body.departureAirportId ||
+   !req.body.arrivalAirportId ||
+   !req.body.arrivalTime ||
+   !req.body.departureTime ||
+   !req.body.price 
+){
+    // if any of the body params is missing w ecome inside it
+    return res.status(400).json({
+        data:{},
+        success:false,
+        message:'invalid request body for creating the flight',
+        err:'missing mandatory properties to create a flight'
+    })
+}
+
+next();
+
+}
+
+module.exports={
+    validateCreateFlight
+}
